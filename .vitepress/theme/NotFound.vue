47dde76b9f49885cac7da50699ed84bba76334a0
Arthur Danjou <arthurdanjou@outlook.fr>
2021-05-20 22:09:45 +0200

    Fix a missing translations

--- .vitepress/theme/NotFound.vue
+++ .vitepress/theme/NotFound.vue
@@ -2,7 +2,7 @@
   <div class="theme">
     <h1>404</h1>
     <blockquote>{{ getMsg() }}</blockquote>
-    <a :href="$site.base" aria-label="go to home">Take me home.</a>
+    <a :href="$site.base" aria-label="go to home">Retournez à l'accueil</a>
   </div>
 </template>
 
