5fb942bec76c364178c4137d21a2ae0922c23515
QC-L <github@liqichang.com>
2021-05-20 10:58:03 +0800

    docs(fr): fix conflicts

--- .vitepress/theme/components/NavBar.vue
+++ .vitepress/theme/components/NavBar.vue
@@ -17,13 +17,8 @@
         </a>
       </div>
 
-<<<<<<< HEAD
-      <div class="item">
-        <a class="nav-btn" href="https://github.com/slidevjs/slidev"  target="_blank" aria-label="View GitHub Repo">
-=======
       <div v-if="repo" class="item">
-        <a class="nav-btn" href="https://github.com/slidevjs/slidev" target="_blank" aria-label="View GitHub Repo">
->>>>>>> c15fbaec822f4d7fb017d6041da8ac689a1d74ce
+        <a class="nav-btn" href="https://github.com/slidevjs/slidev"  target="_blank" aria-label="View GitHub Repo">
           <ri-github-line />
         </a>
       </div>
